cf24e364112dbc7b70acac3892267a333fa727fd
Saul Gabriel <saulgabriel069@gmail.com>
2023-10-26 15:00:53 -0300

    Commit at 26/10

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const app = express()
 
 const caminhoBase = path.join(__dirname, "templates")
 
+app.get('/usuarios/:id', (requisicao, resposta) => {
+    const id = requisicao.params.id
+
+    console.log(`Acessando dados do usuario ${id}`)
+
+    resposta.sendFile(`${caminhoBase}/usuarios.html`)
+})
+
 app.get('/', (requisicao, resposta) => {
     resposta.sendFile(`${caminhoBase}/index.html`)
 })
